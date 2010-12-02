e2e56cb372533d936c0b1eb771600c2c44411fbe
shuhei kinukawa <kinushu@gmail.com>
2010-12-03 00:50:18 +0900

    version 0.10p1

--- Debug/src/CWaveFileWriter.d
+++
@@ -1,6 +0,0 @@
-src/CWaveFileWriter.d src/CWaveFileWriter.o: ../src/CWaveFileWriter.cpp \
-  ../src/CWaveFileWriter.h ../src/CWavFileReader.h
-
-../src/CWaveFileWriter.h:
-
-../src/CWavFileReader.h:
