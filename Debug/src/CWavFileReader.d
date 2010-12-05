cd6aadc297a6b3958bcf376e61654023ff6e50ce
shuhei kinukawa <kinushu@gmail.com>
2010-12-05 16:03:32 +0900

    no need files deleted

--- Debug/src/CWavFileReader.d
+++
@@ -1,5 +0,0 @@
-src/CWavFileReader.d src/CWavFileReader.o: ../src/CWavFileReader.cpp \
-  ../src/CWavFileReader.h
-
-../src/CWavFileReader.h:
-
--- Debug/src/wavmerge.d
+++
@@ -1,7 +0,0 @@
-src/wavmerge.d src/wavmerge.o: ../src/wavmerge.cpp \
-  ../src/CWavFileReader.h ../src/CWaveFileWriter.h
-
-../src/CWavFileReader.h:
-
-../src/CWaveFileWriter.h:
-
